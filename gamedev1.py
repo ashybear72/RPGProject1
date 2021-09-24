d57f3662cf1a43ae84f057ad3ee927997b47f11b
ashybear72 <83081336+ashybear72@users.noreply.github.com>
2021-09-24 10:25:51 -0500

    Update gamedev1.py
    Added to Lore()

--- gamedev1.py
+++ gamedev1.py
@@ -10,16 +10,31 @@ def begin():
 	character()
 
 def character():
+	print("Now listen, i'm not allowed to tell you too much")
 	print("Finn")
 	print("Jeni")
 	print("Ben")
 	lore()
 
 def lore():
-	sel = input("Chose your character: ").lower()
+	sel = input("Chose your friend: ").lower()
 
 	if(sel == "finn"):
 		print("This is Finn, he is a pretty happy person.")
-		print("He lives with his bestfriend and helps people whenever he can")
+		print("He lives with his Ben and helps people whenever he can")
+		print("He loves adventures but because Bens shy they don't do a lot")
+		print("Finn just wants to have some fun")
+	
+	if(sel == "Ben"):
+		print("So Ben huh? he's pretty cool I guess, most people like Finn though")
+		print("He's kinda shy and likes to keep to himself")
+		print("He always feel's bad because he can't go out with Finn, he has bad anxiety")
+		print("I really hope he can get pass that because this is gonna be boringggg if he can't")
+		
+	if(sel == "Jeni")
+	        print("Jeni, Jeniiiiii, Jennniiii")
+		print("Oh yeah!")
+		print("She's well a lot, very spontaneous, but shes nice")
+		print("She just want's to get away I think")
 
 begin()
